470ab2ca547d06255a2bc461a5fd31db883976d1
Vespertino <Vespertino@PC06>
2019-10-29 21:10:26 +0100

    Modificaciones varias en el proyecto
    - Creación clase Implementación
    - Modificación clase Operaciones a abstracta
    - Modificación clase Formulario
    - Se deja de utilizar la clase Principal

--- src/prjgrupo1/Formulario.java
+++ src/prjgrupo1/Formulario.java
@@ -7,7 +7,6 @@ package prjgrupo1;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Set;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
